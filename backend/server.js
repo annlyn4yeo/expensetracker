e276242d0a5899969da387b93e1517bdefeecee4
Annlyn <annlynanto@gmail.com>
2025-04-12 15:53:47 +0530

    mongodb connection established

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,22 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import mongoose from "mongoose";
+
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.get("/", (req, res) => {
   res.send("API is running...");
